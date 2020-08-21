734d6315bc07606aa8b5d3041b64fdc18a99c589
Sam Elliott <sam@lenary.co.uk>
2020-08-21 11:18:36 +0100

    [hugo-extended] Downgrade to 0.60.0
    Signed-off-by: Sam Elliott <sam@lenary.co.uk>

--- Formula/hugo-extended.rb
+++ Formula/hugo-extended.rb
@@ -1,9 +1,8 @@
 class HugoExtended < Formula
   desc "Configurable static site generator (Extended Version)"
   homepage "https://gohugo.io"
-  url "https://github.com/gohugoio/hugo/releases/download/v0.64.0/hugo_extended_0.64.0_macOS-64bit.tar.gz"
-  version "0.64.0"
-  sha256 "c31fbe48ec5a3c1b677abd2755a6fab2ae3baa7536382499243dc21ffd9ebd0e"
+  url "https://github.com/gohugoio/hugo/releases/download/v0.60.0/hugo_extended_0.60.0_macOS-64bit.tar.gz"
+  version "0.60.0"
 
   conflicts_with "hugo", :because => "hugo-extended also installs a hugo binary"
 
