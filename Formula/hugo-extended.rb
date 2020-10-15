99febee98ccbbee709a68eafd4fa0aba6187cd5d
Sam Elliott <sam@lenary.co.uk>
2020-10-15 17:20:41 +0100

    [hugo-extended]
    Signed-off-by: Sam Elliott <sam@lenary.co.uk>

--- Formula/hugo-extended.rb
+++ Formula/hugo-extended.rb
@@ -3,6 +3,7 @@ class HugoExtended < Formula
   homepage "https://gohugo.io"
   url "https://github.com/gohugoio/hugo/releases/download/v0.60.0/hugo_extended_0.60.0_macOS-64bit.tar.gz"
   version "0.60.0"
+  sha256 "3a23d1003f567e81981ab9139c994c1107807008015b01fcfd8500cf9a0b9241"
 
   conflicts_with "hugo", :because => "hugo-extended also installs a hugo binary"
 
